c205e9f7e693d171a93ac0209f3d18cd2dc78caa
bnazari <324817+bnazari@users.noreply.github.com>
2019-03-23 01:57:42 -0700

    Update install.sh

--- install.sh
+++ install.sh
@@ -3,7 +3,7 @@ chmod +x install-allstarlink-repository
 sudo ./install-allstarlink-repository
 sudo apt-get -y update
 sudo apt-get -y upgrade
-sudo apt-get  -y install raspberrypi-kernel-headers dvswitch python-gpiozero python-numpy libasound2-dev python-dev
+sudo apt-get  -y install raspberrypi-kernel-headers dvswitch python-gpiozero python-numpy libasound2-dev python-dev bluetooth bluez blueman pi-bluetooth 
 curl https://bootstrap.pypa.io/get-pip.py -o get-pip.py
 sudo python get-pip.py
 sudo pip install pyalsaaudio
@@ -14,12 +14,10 @@ chmod +x vol.sh
 sudo cp pytalk.service /etc/systemd/system/pytalk.service
 sudo cp MMDVM_Bridge.ini /opt/MMDVM_Bridge/MMDVM_Bridge.ini
 sudo cp Analog_Bridge.ini /opt/Analog_Bridge/Analog_Bridge.ini
-sudo wget -O mic mic.raspiaudio.com
-sudo wget -O test test.raspiaudio.com
+
 
 sudo systemctl enable mmdvm_bridge.service 
 sudo systemctl enable analog_bridge.service
 sudo systemctl enable md380-emu.service
 sudo systemctl enable pytalk
 
-sudo bash mic
