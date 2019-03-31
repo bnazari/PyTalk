7b35f93c3c1fe07918e8b614bf6db9620cb776f6
bnazari <324817+bnazari@users.noreply.github.com>
2019-03-30 23:58:15 -0700

    Update install.sh

--- install.sh
+++ install.sh
@@ -3,7 +3,8 @@ chmod +x install-allstarlink-repository
 sudo ./install-allstarlink-repository
 sudo apt-get -y update
 sudo apt-get -y upgrade
-sudo apt-get  -y install raspberrypi-kernel-headers dvswitch python-gpiozero python-numpy libasound2-dev python-dev bluetooth bluez blueman pi-bluetooth autopoint autoconf libtool libcap-dev libdbus-1-dev libbluetooth-dev bluez libsbc-dev libudev-dev libspeex-dev
+sudo apt-get  -y install raspberrypi-kernel-headers dvswitch python-gpiozero python-numpy libasound2-dev python-dev bluetooth bluez blueman pi-bluetooth 
+# autopoint autoconf libtool libcap-dev libdbus-1-dev libbluetooth-dev bluez libsbc-dev libudev-dev libspeex-dev
  
 curl https://bootstrap.pypa.io/get-pip.py -o get-pip.py
 sudo python get-pip.py
@@ -20,5 +21,5 @@ sudo cp Analog_Bridge.ini /opt/Analog_Bridge/Analog_Bridge.ini
 sudo systemctl enable mmdvm_bridge.service 
 sudo systemctl enable analog_bridge.service
 sudo systemctl enable md380-emu.service
-sudo systemctl enable pytalk
+# sudo systemctl enable pytalk
 
