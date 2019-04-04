22bd656801cdcfa2370fdcac67e08d2af5ef876c
bnazari <324817+bnazari@users.noreply.github.com>
2019-04-04 15:41:18 -0700

    Update pytalk.py

--- pytalk.py
+++ pytalk.py
@@ -111,7 +111,6 @@ def txAudioStream():
                 seq = seq + 1
                 print 'PTT: {}'.format(ptt)
                 bt_tx=0
-                q.close()
             lastPtt = ptt
             if ptt:
                 if bt_tx==0:
@@ -127,20 +126,25 @@ def txAudioStream():
                 seq = seq + 1
         except:
             print("overflow")
-        sleep(.01)
+        sleep(0.02)
+
 ptt = False     # toggle this to transmit (left up to you)
 ser = Serial('/dev/rfcomm0', 9600)
 
 thread.start_new_thread( rxAudioStream, () )
 thread.start_new_thread( txAudioStream, () )
-
 ptt_button=''
 while True:
     sleep(0.02)
     ptt_button = ser.read(6)
     if (ptt_button=='+PTT=P'):
       ptt=True
+      q = alsaaudio.PCM(type=alsaaudio.PCM_CAPTURE)
+      q.setformat(alsaaudio.PCM_FORMAT_S16_LE)
+      q.setrate(8000)
+      q.setchannels(1) 
       ptt_button=''
     if (ptt_button=='+PTT=R'):
       ptt=False
+      q.close()
       ptt_button=''
